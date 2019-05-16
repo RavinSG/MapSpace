ecd65dd9c09dfc076e56d77bd0cbbb43c06224ba
Ravin Gunawardena <RavinSG@users.noreply.github.com>
2019-05-16 11:42:53 +0530

    Tweak draw component

--- src/app/draw/draw.component.ts
+++ src/app/draw/draw.component.ts
@@ -1,7 +1,8 @@
-import {Component, EventEmitter, OnInit} from '@angular/core';
+import {Component, DoCheck, KeyValueDiffer, KeyValueDiffers, OnInit} from '@angular/core';
 import {Params, ActivatedRoute} from '@angular/router';
 import {switchMap} from 'rxjs/operators';
-import {MatButtonToggleChange} from '@angular/material/typings/esm5/button-toggle';
+import {MatButtonToggleChange, MatSnackBar} from '@angular/material';
+
 
 declare const google: any;
 
@@ -10,9 +11,13 @@ declare const google: any;
   templateUrl: './draw.component.html',
   styleUrls: ['./draw.component.scss']
 })
-export class DrawComponent {
+export class DrawComponent implements OnInit, DoCheck {
+
+  public coordinates = {val: 1};
 
   mapType = 'hybrid';
+  private pathDiffer: KeyValueDiffer<string, any>;
+  private enableGeo: boolean;
 
   details = {
     'lng': 0,
@@ -25,21 +30,47 @@ export class DrawComponent {
     zoom: 16
   };
 
-  constructor(private route: ActivatedRoute) {
+
+  constructor(private route: ActivatedRoute,
+              private differs: KeyValueDiffers,
+              private snackBar: MatSnackBar) {
 
   }
 
   ngOnInit() {
+    this.pathDiffer = this.differs.find(this.coordinates).create();
+
     if (this.route.snapshot.queryParams['lng']) {
-      const result1 = this.route.params.pipe(
+      this.route.params.pipe(
         switchMap((params: Params) => this.details['lng'] = params['lng'])
       ).subscribe();
-      const result2 = this.route.params.pipe(
+      this.route.params.pipe(
         switchMap((params: Params) => this.details['lat'] = params['lat'])
       ).subscribe();
       this.center.lat = Number(this.details.lat);
       this.center.lng = Number(this.details.lng);
       console.log(typeof (Number(this.details.lat)));
+    }
+  }
+
+  ngDoCheck(): void {
+    const changes = this.pathDiffer.diff(this.coordinates);
+    if (changes) {
+      console.log(this.coordinates);
+    }
+  }
+
+  geoChange(event) {
+    this.enableGeo = event.checked;
+    console.log(this.enableGeo);
+    if (this.enableGeo) {
+      this.snackBar.open('Sphere geometry is enabled', 'OK', {
+        duration: 1000
+      });
+    } else {
+      this.snackBar.open('Sphere geometry is disabled', 'OK', {
+        duration: 1000
+      });
     }
   }
 
@@ -49,6 +80,7 @@ export class DrawComponent {
 
 
   initDrawingManager(map: any) {
+
     const options = {
       drawingControl: true,
       drawingControlOptions: {
@@ -64,42 +96,51 @@ export class DrawComponent {
 
     const drawingManager = new google.maps.drawing.DrawingManager(options);
     drawingManager.setMap(map);
-    google.maps.event.addListener(drawingManager, 'overlaycomplete', function (shape) {
+
+    function eventHandler(shape, coords) {
+
       function createPath(pathShape) {
-        const coordinates = [];
+        const pCoordinates = [];
         const len = pathShape.getPath().getLength();
         const path = pathShape.getPath();
         for (let i = 0; i < len; i++) {
           const vertex = path.getAt(i);
-          coordinates.push(vertex.lat(), vertex.lng());
+          pCoordinates.push(vertex.lat(), vertex.lng());
         }
-        return coordinates;
+        return pCoordinates;
       }
+
 
       if (shape.type === 'polygon') {
         const newShape = shape.overlay;
         newShape.type = shape.type;
-        console.log(newShape);
-        let coordinates = createPath(newShape);
-        console.log(coordinates);
-
-        google.maps.event.addListener(newShape.getPath(), 'dragend', function () {
-          console.log('testing');
+        // console.log(newShape);
+        coords.val = createPath(newShape);
+        // console.log(coords);
+        google.maps.event.addListener(newShape, 'dragend', function () {
+          coords.val = createPath(newShape);
         });
         newShape.getPaths().forEach(function (path, index) {
-
           google.maps.event.addListener(path, 'set_at', function () {
-            coordinates = createPath(newShape);
-            console.log(coordinates);
+            coords.val = createPath(newShape);
           });
+
           google.maps.event.addListener(path, 'insert_at', function () {
-            coordinates = createPath(newShape);
-            console.log(coordinates);
+            coords.val = createPath(newShape);
+          });
+
+          google.maps.event.addListener(path, 'remove_at', function () {
+            coords.val = createPath(newShape);
           });
 
         });
       }
-    });
+      console.log(event, 'here', coords);
+    }
+
+    google.maps.event.addListener(drawingManager, 'overlaycomplete',
+      (event) => eventHandler(event, this.coordinates));
+
     google.maps.event.addListener(drawingManager, 'drawingmode_changed', function (e) {
       console.log('drawing mode changed:' + drawingManager.getDrawingMode());
     });
@@ -108,6 +149,7 @@ export class DrawComponent {
   mapChange(event: MatButtonToggleChange) {
     this.mapType = event.value;
     console.log(event.value);
+    console.log(this.coordinates);
   }
 
 
